96c51c0a2f828a6ab843049274182611a8d66125
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-01-05 23:57:20 +0530

    Styled ServiceSection

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -7,49 +7,97 @@ import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import home2 from "../img/home2.png";
 
+// Importing Styles
+import styled from "styled-components";
+import {
+  StyledLayout,
+  StyledDescription,
+  StyledImage,
+} from "../styles/layoutStyles";
+
 const ServicesSection = () => {
   return (
-    <div className="services">
-      <div className="description">
+    <StyledServices>
+      <StyledDescription>
         <h2>
-          High
-          <span>quality</span>
-          services
+          High <span>quality</span> services
         </h2>
-        <div className="card">
-          <div className="icon">
-            <img src={clock} alt="clock icon" />
-            <h3>Efficient</h3>
-          </div>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-        <div className="card">
-          <div className="icon">
-            <img src={teamwork} alt="teamwork icon" />
-            <h3>Teamwork</h3>
-          </div>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-        <div className="card">
-          <div className="icon">
-            <img src={diaphragm} alt="diaphragm icon" />
-            <h3>Diaphragm</h3>
-          </div>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-        <div className="card">
-          <div className="icon">
-            <img src={money} alt="money icon" />
-            <h3>Affordable</h3>
-          </div>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-      </div>
-      <div className="image">
+        <StyledCards>
+          <StyledCard>
+            <div className="icon">
+              <img src={clock} alt="clock icon" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </StyledCard>
+          <StyledCard>
+            <div className="icon">
+              <img src={teamwork} alt="teamwork icon" />
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </StyledCard>
+          <StyledCard>
+            <div className="icon">
+              <img src={diaphragm} alt="diaphragm icon" />
+              <h3>Diaphragm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </StyledCard>
+          <StyledCard>
+            <div className="icon">
+              <img src={money} alt="money icon" />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </StyledCard>
+        </StyledCards>
+      </StyledDescription>
+      <StyledImage>
         <img src={home2} alt="home2" />
-      </div>
-    </div>
+      </StyledImage>
+    </StyledServices>
   );
 };
+
+// Styles
+const StyledServices = styled(StyledLayout)`
+  min-height: 90vh;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5rem 10rem;
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+
+const StyledCards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const StyledCard = styled.div`
+  flex-basis: 15rem;
+  .icon {
+    display: flex;
+    align-items: center;
+
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+  p {
+    font-size: 1rem;
+  }
+`;
 
 export default ServicesSection;
