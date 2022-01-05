7a8c07baeedec5b19d70ec18aedec2eb6b61d0a0
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-01-06 00:10:59 +0530

    Styled Faq

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,12 @@
 import React from "react";
 
+// Import Style
+import styled from "styled-components";
+import { StyledLayout } from "../styles/layoutStyles";
+
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <StyledFaq>
       <h2>
         Any Questions? <span>FAQ</span>
       </h2>
@@ -16,6 +20,7 @@ const FaqSection = () => {
             optio.
           </p>
         </div>
+        <div className="faq-line" />
       </div>
 
       <div className="question">
@@ -27,6 +32,7 @@ const FaqSection = () => {
             optio.
           </p>
         </div>
+        <div className="faq-line" />
       </div>
 
       <div className="question">
@@ -38,6 +44,7 @@ const FaqSection = () => {
             optio.
           </p>
         </div>
+        <div className="faq-line" />
       </div>
 
       <div className="question">
@@ -49,9 +56,35 @@ const FaqSection = () => {
             optio.
           </p>
         </div>
+        <div className="faq-line" />
       </div>
-    </div>
+    </StyledFaq>
   );
 };
+
+// Styles
+const StyledFaq = styled(StyledLayout)`
+  display: block;
+  span {
+    display: block;
+    color: #23d997;
+  }
+  h2 {
+    font-weight: lighter;
+    padding-bottom: 4rem;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
