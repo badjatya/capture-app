48b13c0df697747660314f7689c5685d8a069f93
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-01-07 19:01:28 +0530

    Animating About section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,29 +3,66 @@ import React from "react";
 // Importing images
 import home1 from "../img/home1.png";
 
-// Importing styled
+// Importing Lib
 import styled from "styled-components";
+import { motion } from "framer-motion";
 import {
   StyledLayout,
   StyledDescription,
   StyledImage,
 } from "../styles/layoutStyles";
 
+// const AnimateDescription = {
+//   hidden: { x: 100 },
+//   show: {
+//     x: 0,
+//     transition: {
+//       duration: 1,
+//       ease: "easeOut",
+//       staggerChildren: 0.5,
+//     },
+//   },
+// };
+
+// const AnimateTitle = {
+//   hidden: { opacity: 0 },
+//   show: {
+//     opacity: 1,
+//     transition: { duration: 2 },
+//   },
+// };
+
 const AboutSection = () => {
   return (
     <StyledAbout>
       <StyledDescription>
-        <StyledHide>
-          <h2>We work to make</h2>
-        </StyledHide>
-        <StyledHide>
-          <h2>
-            your <span>dreams</span>
-          </h2>
-        </StyledHide>
-        <StyledHide>
-          <h2>come true.</h2>
-        </StyledHide>
+        <motion.div
+        // variants={AnimateDescription}
+        // initial="hidden"
+        // animate="show"
+        >
+          <StyledHide>
+            <motion.h2
+            // variants={AnimateTitle}
+            >
+              We work to make
+            </motion.h2>
+          </StyledHide>
+          <StyledHide>
+            <motion.h2
+            // variants={AnimateTitle}
+            >
+              your <span>dreams</span>
+            </motion.h2>
+          </StyledHide>
+          <StyledHide>
+            <motion.h2
+            // variants={AnimateTitle}
+            >
+              come true.
+            </motion.h2>
+          </StyledHide>
+        </motion.div>
         <p>
           Contact us for any photography or video graph ideas that you have. We
           have professionals with amazing skills to help you achieve it.{" "}
