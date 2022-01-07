25b4adae3796875c7ec65c2487d3a919599b1d89
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-01-07 19:29:06 +0530

    Adding page transition on Contact us

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+// Import Lib
+import { motion } from "framer-motion";
+import { pageAnimation } from "../styles/animations";
+
 const ContactUs = () => {
   return (
-    <div>
-      <h1>Contact Us page</h1>
-    </div>
+    <motion.div
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+    >
+      <h1 style={{ marginTop: "10vh" }}>Contact Us page</h1>
+    </motion.div>
   );
 };
 
