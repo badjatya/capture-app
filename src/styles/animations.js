6d7cbe01c929a3fe67fd7d29e6fb8943a39e2cb9
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-01-08 18:41:03 +0530

    Creating Wave animation

--- src/styles/animations.js
+++ src/styles/animations.js
@@ -63,3 +63,19 @@ export const photoAnimation = {
     },
   },
 };
+
+export const waveAnimation = {
+  hidden: {
+    pathLength: 0,
+    pathOffset: 1,
+  },
+  show: {
+    pathLength: 1,
+    pathOffset: 0,
+    transition: {
+      duration: 1,
+      ease: "easeOut",
+      delay: 0.5,
+    },
+  },
+};
