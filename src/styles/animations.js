4039e58b838ddff27377c85e60f316fd270dc828
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-01-11 22:17:01 +0530

    Animating OurWork

--- src/styles/animations.js
+++ src/styles/animations.js
@@ -92,3 +92,27 @@ export const lineAnimation = {
     },
   },
 };
+
+export const slider = {
+  hidden: { x: "-130%", skew: "45deg" },
+  show: {
+    x: "100%",
+    skew: "0deg",
+    transition: { type: "tween", ease: "easeOut", duration: 1 },
+  },
+};
+
+export const sliderContainer = {
+  hidden: {
+    opacity: 1,
+  },
+  show: {
+    opacity: 1,
+
+    transition: {
+      staggerChildren: 0.15,
+      ease: "easeOut",
+      duration: 1,
+    },
+  },
+};
